38af802be495210ada3cf38281365635ed516634
pppbigbud <pppbigbud@gmail.com>
2022-10-25 09:11:33 +0200

    GiteBase avec BDD ok

---
+++ src/Entity/Fournisseur.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FournisseurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FournisseurRepository::class)]
+class Fournisseur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $code_postal = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $siret = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $telephone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $contact_name = null;
+
+    #[ORM\OneToMany(mappedBy: 'fournisseur', targetEntity: Product::class)]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?int
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(?int $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getSiret(): ?float
+    {
+        return $this->siret;
+    }
+
+    public function setSiret(?float $siret): self
+    {
+        $this->siret = $siret;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?float
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?float $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getContactName(): ?string
+    {
+        return $this->contact_name;
+    }
+
+    public function setContactName(?string $contact_name): self
+    {
+        $this->contact_name = $contact_name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setFournisseur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getFournisseur() === $this) {
+                $product->setFournisseur(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $priceHT = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_At = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Img $img = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Categorie $categorie = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Caracteristique $caracteristique = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Tva $tva = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Fournisseur $fournisseur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPriceHT(): ?float
+    {
+        return $this->priceHT;
+    }
+
+    public function setPriceHT(float $priceHT): self
+    {
+        $this->priceHT = $priceHT;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_At;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_At): self
+    {
+        $this->created_At = $created_At;
+
+        return $this;
+    }
+
+    public function getImg(): ?Img
+    {
+        return $this->img;
+    }
+
+    public function setImg(?Img $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getCaracteristique(): ?Caracteristique
+    {
+        return $this->caracteristique;
+    }
+
+    public function setCaracteristique(?Caracteristique $caracteristique): self
+    {
+        $this->caracteristique = $caracteristique;
+
+        return $this;
+    }
+
+    public function getTva(): ?Tva
+    {
+        return $this->tva;
+    }
+
+    public function setTva(?Tva $tva): self
+    {
+        $this->tva = $tva;
+
+        return $this;
+    }
+
+    public function getFournisseur(): ?Fournisseur
+    {
+        return $this->fournisseur;
+    }
+
+    public function setFournisseur(?Fournisseur $fournisseur): self
+    {
+        $this->fournisseur = $fournisseur;
+
+        return $this;
+    }
+}
