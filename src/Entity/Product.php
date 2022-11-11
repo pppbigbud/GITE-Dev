d605d8d309d6e2a7c9c300592f33f0eb30242b77
pppbigbud <pppbigbud@gmail.com>
2022-11-11 10:14:33 +0100

    easy admin install + firstEntity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -14,7 +14,11 @@ class Product
     #[ORM\Column]
     private ?int $id = null;
 
+<<<<<<< HEAD
     #[ORM\Column(length: 255)]
+=======
+    #[ORM\Column(length: 50)]
+>>>>>>> 411be73 (easy admin install + firstEntity)
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
@@ -23,6 +27,7 @@ class Product
     #[ORM\Column]
     private ?float $priceHT = null;
 
+<<<<<<< HEAD
     #[ORM\Column]
     private ?\DateTimeImmutable $created_At = null;
 
@@ -40,6 +45,28 @@ class Product
 
     #[ORM\ManyToOne(inversedBy: 'products')]
     private ?Fournisseur $fournisseur = null;
+=======
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $img = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vendor = null;
+
+    #[ORM\Column]
+    private ?float $tva = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $stock = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $category = null;
+>>>>>>> 411be73 (easy admin install + firstEntity)
 
     public function getId(): ?int
     {
@@ -82,6 +109,7 @@ class Product
         return $this;
     }
 
+<<<<<<< HEAD
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->created_At;
@@ -90,22 +118,53 @@ class Product
     public function setCreatedAt(\DateTimeImmutable $created_At): self
     {
         $this->created_At = $created_At;
+=======
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+>>>>>>> 411be73 (easy admin install + firstEntity)
 
         return $this;
     }
 
+<<<<<<< HEAD
     public function getImg(): ?Img
+=======
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+>>>>>>> 411be73 (easy admin install + firstEntity)
     {
         return $this->img;
     }
 
+<<<<<<< HEAD
     public function setImg(?Img $img): self
+=======
+    public function setImg(?string $img): self
+>>>>>>> 411be73 (easy admin install + firstEntity)
     {
         $this->img = $img;
 
         return $this;
     }
 
+<<<<<<< HEAD
     public function getCategorie(): ?Categorie
     {
         return $this->categorie;
@@ -114,10 +173,21 @@ class Product
     public function setCategorie(?Categorie $categorie): self
     {
         $this->categorie = $categorie;
+=======
+    public function getVendor(): ?string
+    {
+        return $this->vendor;
+    }
+
+    public function setVendor(?string $vendor): self
+    {
+        $this->vendor = $vendor;
+>>>>>>> 411be73 (easy admin install + firstEntity)
 
         return $this;
     }
 
+<<<<<<< HEAD
     public function getCaracteristique(): ?Caracteristique
     {
         return $this->caracteristique;
@@ -131,17 +201,25 @@ class Product
     }
 
     public function getTva(): ?Tva
+=======
+    public function getTva(): ?float
+>>>>>>> 411be73 (easy admin install + firstEntity)
     {
         return $this->tva;
     }
 
+<<<<<<< HEAD
     public function setTva(?Tva $tva): self
+=======
+    public function setTva(float $tva): self
+>>>>>>> 411be73 (easy admin install + firstEntity)
     {
         $this->tva = $tva;
 
         return $this;
     }
 
+<<<<<<< HEAD
     public function getFournisseur(): ?Fournisseur
     {
         return $this->fournisseur;
@@ -150,6 +228,28 @@ class Product
     public function setFournisseur(?Fournisseur $fournisseur): self
     {
         $this->fournisseur = $fournisseur;
+=======
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?string $category): self
+    {
+        $this->category = $category;
+>>>>>>> 411be73 (easy admin install + firstEntity)
 
         return $this;
     }
